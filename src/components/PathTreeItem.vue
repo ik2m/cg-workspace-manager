3fb185d534dd0735db1ed76ddadd066c9e95a235
ik2m <39111330+ik2m@users.noreply.github.com>
2025-04-14 21:20:05 +0900

    feat: Add Material Icons and integrate into PathTreeItem component

--- src/components/PathTreeItem.vue
+++ src/components/PathTreeItem.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { PathTree } from "../types";
 import PathTreeItem from "./PathTreeItem.vue";
+import MaterialIcon from "./MaterialIcon.vue";
 
 defineProps<{
   pathTree: PathTree;
@@ -16,7 +17,8 @@ const toggleChildren = () => {
 
 <template>
   <div>
-    <span @click="toggleChildren">
+    <span @click="toggleChildren" class="text-md">
+    <MaterialIcon :name="showChildren ? 'keyboard_arrow_down': 'keyboard_arrow_right'" />
     {{ pathTree.name }}
     </span>
     <div v-if=" pathTree.children && showChildren" class="pl-2">
