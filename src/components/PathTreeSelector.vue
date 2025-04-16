ac8becda5cac0e0148a2d0452e3c7ae35ab177a6
ik2m <39111330+ik2m@users.noreply.github.com>
2025-04-17 00:37:21 +0900

    Add selection tracking and highlighting for PathTree items

--- src/components/PathTreeSelector.vue
+++ src/components/PathTreeSelector.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
 import PathTreeSelectorItem from "./PathTreeSelectorItem.vue";
 import {PathTree} from "../types";
+import {computed} from "vue";
 
-defineProps<{
+const props = defineProps<{
   pathTree: PathTree;
 }>()
+
+const colorClass = computed(()=> {
+  return selectedPath.value === props.pathTree.path ? 'bg-accent' : '';
+});
+
+const selectedPath = defineModel<string|null>({ default: null })
 </script>
 
 <template>
   <div>
-    <button class="btn btn-ghost text-md cursor-pointe w-full justify-start">
+    <button class="btn btn-ghost text-md cursor-pointe w-full justify-start" @click="selectedPath=pathTree.path" :class="colorClass">
       {{ pathTree.name }}
     </button>
     <ul v-for="child in pathTree.children" :key="pathTree.path">
-      <PathTreeSelectorItem v-if="child" :path-tree="child"/>
+      <PathTreeSelectorItem
+          v-if="child"
+          :path-tree="child"
+          :depth="0"
+          :selected-path="selectedPath"
+          @select="selectedPath = $event"
+      />
     </ul>
   </div>
 </template>
--- src/components/PathTreeSelectorItem.vue
+++ src/components/PathTreeSelectorItem.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import {computed, ref} from "vue";
 import { Dir, File } from "../types";
 import PathTreeItem from "./PathTreeSelectorItem.vue";
 import MaterialIcon from "./MaterialIcon.vue";
 
-withDefaults(defineProps<{
+const props = defineProps<{
   pathTree: Dir|File;
-  depth?: number;
-}>(),{
-  depth: 0
-})
+  depth: number;
+  selectedPath: string|null;
+}>()
+
+const emits = defineEmits([
+  'select'
+])
+
+
 
 const showChildren = ref<boolean>(false);
 
-const toggleChildren = () => {
+const colorClass = computed(()=> {
+  return props.selectedPath === props.pathTree.path ? 'bg-accent' : '';
+});
+
+const onClick = () => {
   showChildren.value = !showChildren.value;
+  emits('select', props.pathTree.path);
 };
 </script>
 
 <template>
   <li>
-    <button @click="toggleChildren" class="btn btn-ghost text-md cursor-pointe w-full justify-start">
+    <button @click="onClick" class="btn btn-ghost text-md cursor-pointe w-full justify-start rounded-selector" :class="colorClass">
       <span class="inline-flex items-center">
       <span :style="{ 'width': `${depth * 8}px` }" />
       <MaterialIcon v-if="pathTree.type === 'dir'" :name="showChildren ? 'keyboard_arrow_down': 'keyboard_arrow_right'" />
@@ -28,7 +38,9 @@ const toggleChildren = () => {
       </span>
     </button>
     <ul v-if="pathTree.type === 'dir' && showChildren">
-      <PathTreeItem v-for="child in pathTree.children" :key="child.path" :path-tree="child" :depth="depth + 1" />
+      <PathTreeItem v-for="child in pathTree.children" :key="child.path" :path-tree="child" :depth="depth + 1" :selected-path="selectedPath"
+        @select="emits('select', $event)"
+      />
       <li v-if="pathTree.children.length === 0">データはありません</li>
     </ul>
   </li>
