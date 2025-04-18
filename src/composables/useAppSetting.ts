b8a48e4f0f9a5afaa4eecc40240b9bf3b8204ee1
ik2m <39111330+ik2m@users.noreply.github.com>
2025-04-18 18:37:46 +0900

    fixトップレベルのawaitをやめた

--- src/composables/useAppSetting.ts
+++ src/composables/useAppSetting.ts
@@ -3,22 +3,26 @@ import { load } from "@tauri-apps/plugin-store";
 
 const SETTING_KEY_WORKSPACE_DIR = "workspace-dir";
 
-const store = await load("store.json", { autoSave: false });
 const initialLoaded = ref<boolean>(false);
 
 const workspaceDir = ref<string | null>(null);
 
 export const useAppSetting = () => {
-  if (!initialLoaded.value) {
+  const init = async () => {
+    const store = await load("store.json", { autoSave: false });
     store.get<{ value: string }>(SETTING_KEY_WORKSPACE_DIR).then((data) => {
       if (data) {
         workspaceDir.value = data.value;
       }
       initialLoaded.value = true;
     });
+  };
+  if (!initialLoaded.value) {
+    init();
   }
 
   const updateWorkSpaceDir = async (dir: string) => {
+    const store = await load("store.json", { autoSave: false });
     await store.set(SETTING_KEY_WORKSPACE_DIR, { value: dir });
     await store.save();
     workspaceDir.value = dir;
