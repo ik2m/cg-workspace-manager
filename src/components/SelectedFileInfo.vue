954334908ee857c3a142ad43c8f9f56004bd57c9
ik2m <39111330+ik2m@users.noreply.github.com>
2025-04-18 14:54:27 +0900

    Add support for opening files via `openPath` in UI

--- src/components/SelectedFileInfo.vue
+++ src/components/SelectedFileInfo.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { defineProps } from "vue";
+import { openPath } from "@tauri-apps/plugin-opener";
 
-defineProps<{
+const props = defineProps<{
   path: string;
 }>();
+
+const openFile = () => {
+  openPath(props.path);
+};
 </script>
 
 <template>
   <div class="bg-accent rounded-md p-1 m-1">
-    <span class="line-clamp-1">{{ path }}</span>
-    <button class="btn">open</button>
+    <span class="line-clamp-1">{{ props.path }}</span>
+    <button class="btn" @click="openFile">open</button>
   </div>
 </template>
